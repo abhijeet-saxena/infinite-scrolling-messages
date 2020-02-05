892f5eb857a811c7f4b47ab7c9208d98ebaf1f25
Abhijeet Saxena <saxenaabhijeet@outlook.com>
2020-02-06 01:37:17 +0530

    feat(app): Add Swipe to dismiss feature

--- src/index.js
+++ src/index.js
@@ -1,90 +1,117 @@
-import { getRandomTimestamp } from "./scripts/helpers";
+import {
+  setTimeInStatusBar,
+  generateMessageCardsHTML
+} from "./scripts/helpers";
 
-//Global Variables
-let token = "";
-const time = document.querySelector(".time");
-const loader = document.getElementById("loader");
-const wrapper = document.querySelector(".card-wrapper");
-const content = document.querySelector(".content");
+window.addEventListener("load", async () => {
+  // Global Variables
+  let token = "";
+  const loader = document.getElementById("loader");
+  const wrapper = document.querySelector(".card-wrapper");
+  const content = document.querySelector(".content");
+  let lastDismissedCardID = "0";
 
-//This all is needed for infinite scrolling
-const options = {
-  root: content,
-  rootMargin: "400px 0px",
-  threshold: 0
-};
+  // This all is needed for infinite scrolling
+  const options = {
+    root: content,
+    rootMargin: "400px 0px",
+    threshold: 0
+  };
 
-let callback = (entries, observer) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      loader.classList.toggle("active");
-      setTimeout(() => {
-        getMessages();
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(async entry => {
+      // This simply updates the timestamp in status bar
+      setTimeInStatusBar();
+
+      // If loader is close to viewport, show loading spinner and load the next batch of messages
+      if (entry.isIntersecting) {
         loader.classList.toggle("active");
-      }, 500);
-    }
-  });
-};
-
-let observer = new IntersectionObserver(callback, options);
-
-let generateCard = messageList => {
-  messageList.forEach(msg => {
-    let {
-      content,
-      id,
-      author: { name, photoUrl }
-    } = msg;
-    let card = `
-<div class="card" id="${id}">
-  <div class="lead">
-    <img class="avatar" width="40" alt="${name} Avatar" height="40" src="https://message-list.appspot.com${photoUrl}"/>      
-    <div class="author-data">
-      <h4>${name}</h4>
-      <span> ${getRandomTimestamp()} </span>
-    </div>
-  </div>
-  <div class="message">
-    ${content.length < 200 ? content : `${content.slice(0, 200)}...`}
-   </div>
-</div>
-    `;
-    wrapper.innerHTML += card;
-  });
-};
-
-//Initialisation script
-let getMessages = () => {
-  fetch(`https://message-list.appspot.com/messages?pageToken=${token}`)
-    .then(res => res.json())
-    .then(res => {
-      generateCard(res.messages);
-      if (!token) observer.observe(loader);
-      token = res.pageToken;
+        token = await getMessagesFromServer();
+        loader.classList.toggle("active");
+      }
     });
-};
+  }, options);
 
-getMessages();
+  const getMessagesFromServer = () => {
+    return fetch(`https://message-list.appspot.com/messages?pageToken=${token}`)
+      .then(res => res.json())
+      .then(res => {
+        wrapper.innerHTML += generateMessageCardsHTML(res.messages);
 
-// For swipe Animation
-wrapper.addEventListener(
-  "click",
-  event => {
-    let clicked_card = event.target.closest(".card");
-    if (clicked_card) {
-      clicked_card.classList.add("dismissing");
+        // Attach observer for the first time load only
+        if (!token) observer.observe(loader);
+        return res.pageToken;
+      });
+  };
 
-      setTimeout(() => {
-        clicked_card.classList.add("dismissed");
-      }, 210);
+  let anchorX = 0; // Used to store the startX of touch point
+  let anchorY = 0; // Used to store the startY of touch point
 
-      setTimeout(() => {
-        clicked_card.classList.remove("dismissed");
-        clicked_card.classList.remove("dismissing");
-      }, 1000);
-    }
-  },
-  false
-);
+  wrapper.addEventListener(
+    "touchstart",
+    event => {
+      anchorX = event.changedTouches[0].clientX;
+      anchorY = event.changedTouches[0].clientY;
+    },
+    false
+  );
 
-time.innerHTML = `${new Date().getHours()}:${new Date().getMinutes()}`;
+  wrapper.addEventListener(
+    "touchmove",
+    event => {
+      const clicked_card = event.target.closest(".card");
+      const displacementX = event.changedTouches[0].clientX - anchorX;
+      const displacementY = event.changedTouches[0].clientY - anchorY;
+
+      // Set transition duration to 0ms for instant response on swiping
+      clicked_card.style.transitionDuration = "0ms";
+
+      if (displacementX > 0 && Math.abs(displacementY) < 100) {
+        // Swiped in right direction — add transition and translateX card by the displacementX
+        if (!clicked_card.classList.contains("dismissing")) {
+          clicked_card.closest(".card").classList.add("dismissing");
+        }
+        clicked_card.style.transform = `translateX(${displacementX}px)`;
+      } else {
+        // Swiped in left direction — reset card position to left edge
+        clicked_card.style.transform = "translateX(0px)";
+      }
+    },
+    false
+  );
+
+  wrapper.addEventListener(
+    "touchend",
+    event => {
+      const clicked_card = event.target.closest(".card");
+      clicked_card.classList.remove("dismissing");
+
+      // Set transition duration to 200ms for smooth dismissal transition
+      clicked_card.style.transitionDuration = "200ms";
+
+      const displacementX = event.changedTouches[0].clientX - anchorX;
+      const displacementY = event.changedTouches[0].clientX - anchorY;
+
+      if (displacementX > 125 && displacementY < 100) {
+        clicked_card.style.transform = "translateX(100vw)";
+
+        // Hide the card after swiping is complete
+        setTimeout(() => {
+          clicked_card.classList.add("dismissed");
+          lastDismissedCardID = clicked_card.id;
+        }, 200);
+
+        setTimeout(() => {
+          console.log(lastDismissedCardID);
+        }, 2000);
+      } else {
+        // Swiped in left direction — reset card position to left edge
+        clicked_card.style.transform = "translateX(0px)";
+      }
+    },
+    false
+  );
+
+  // APP INITIAIZATION
+  token = await getMessagesFromServer();
+});
--- src/scripts/helpers.js
+++ src/scripts/helpers.js
@@ -1,9 +1,44 @@
-export const getRandomTimestamp = () => {
-  const time = Math.floor(Math.random() * 22);
-  let unit = Math.floor(Math.random() * 2) ? "hour" : "minute";
+export const setTimeInStatusBar = element => {
+  const hours = new Date().getHours();
+  const minutes = new Date().getMinutes();
+  document.querySelector(".time").innerHTML = `${hours}:${minutes}`;
+};
 
-  if (time === 0) return "Just Now";
-  else if (time > 1) unit += "s";
+export const generateMessageCardsHTML = messageList => {
+  const getRandomTimestamp = () => {
+    const time = Math.floor(Math.random() * 22);
+    let unit = Math.floor(Math.random() * 2) ? "hour" : "minute";
 
-  return `${time} ${unit} ago`;
+    if (time === 0) return "A few moments ago";
+    else if (time > 1) unit += "s";
+
+    return `${time} ${unit} ago`;
+  };
+
+  let cardsFragment = "";
+
+  for (let i = 0; i < messageList.length; i++) {
+    let {
+      content,
+      id,
+      author: { name, photoUrl }
+    } = messageList[i];
+
+    cardsFragment += `
+    <div class="card" id="${id}">
+      <div class="lead">
+        <img class="avatar" width="40" alt="${name} Avatar" height="40" src="https://message-list.appspot.com${photoUrl}"/>      
+        <div class="author-data">
+          <h4>${name}</h4>
+          <span> ${getRandomTimestamp()} </span>
+        </div>
+      </div>
+      <div class="message">
+      ${content.length < 200 ? content : `${content.slice(0, 200)}...`}
+      </div>
+    </div>
+    `;
+  }
+
+  return cardsFragment;
 };
